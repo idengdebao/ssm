b3f87d352030e6366c819f0a2cb9d221ef913afa
idengdebao <38874677+idengdebao@users.noreply.github.com>
2018-05-01 16:35:48 +0800

    Update BaseTest.java

--- src/test/java/com/soecode/lyf/BaseTest.java
+++ src/test/java/com/soecode/lyf/BaseTest.java
@@ -5,7 +5,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
- * 配置spring和junit整合，junit启动时加载springIOC容器 spring-test,junit
+ * 配置spring和junit整合，junit启动时加载springIOC容器 spring-test,junit.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 // 告诉junit spring配置文件
